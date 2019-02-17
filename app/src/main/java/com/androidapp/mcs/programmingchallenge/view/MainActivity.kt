a49b27a95ffaaadaa2f5fed088cba8abed372e54
Saudamini Keshav <srjoshi279@gmail.com>
2019-02-16 19:43:54 -0600

    Random Jokes

--- app/src/main/java/com/androidapp/mcs/programmingchallenge/view/MainActivity.kt
+++ app/src/main/java/com/androidapp/mcs/programmingchallenge/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.androidapp.mcs.programmingchallenge
+package com.androidapp.mcs.programmingchallenge.view
 
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.widget.Toast
+import com.androidapp.mcs.programmingchallenge.R
 import com.androidapp.mcs.programmingchallenge.model.RandomJokes
 import com.androidapp.mcs.programmingchallenge.viewmodel.BaseViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -24,10 +25,10 @@ class MainActivity : AppCompatActivity() {
         mViewModel.getJokesFromVM()
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(IoScheduler())
-            .subscribe({ result ->
-                showDialogBox(result)
+            .subscribe({
+                showDialogBox(it)
             },
-                { error ->
+                {
                     Log.i("MainActivity", "Error Occured")
                 })
     }
@@ -35,15 +36,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun showDialogBox(it2: RandomJokes) {
 
-        Toast.makeText(applicationContext, "Recieved", Toast.LENGTH_SHORT)
+        //Toast.makeText(applicationContext, "Recieved", Toast.LENGTH_SHORT)
 
+        val mJokes = it2.value.joke
         random_jokes.setOnClickListener {
             val builder = AlertDialog.Builder(this@MainActivity)
             builder.setTitle("Random Jokes")
-            builder.setMessage(it2.value.joke)
+            builder.setMessage(mJokes)
             builder.setNeutralButton("Dismiss") { dialogInterface, i ->
                 Toast.makeText(applicationContext, "Dismiss", Toast.LENGTH_SHORT).show()
             }
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
 
 
         }
