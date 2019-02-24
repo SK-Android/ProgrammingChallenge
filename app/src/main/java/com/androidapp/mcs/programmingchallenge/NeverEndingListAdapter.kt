76e63f4f426c0d7b81abc2952ae887d778a81bfb
Saudamini Keshav <srjoshi279@gmail.com>
2019-02-23 20:26:55 -0600

    Input Validation

--- app/src/main/java/com/androidapp/mcs/programmingchallenge/NeverEndingListAdapter.kt
+++ app/src/main/java/com/androidapp/mcs/programmingchallenge/NeverEndingListAdapter.kt
@@ -4,15 +4,25 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import com.androidapp.mcs.programmingchallenge.model.Value
+import java.nio.file.Files.size
+
+
 
 class NeverEndingListAdapter(val jokes: List<Value>?): RecyclerView.Adapter<NeverEndingListAdapter.CustomViewHolder>() {
 
+//        val VIEW_TYPE_ITEM = 1
+//        val VIEW_TYPE_PROGRESSBAR = 0
+//        var isFooterEnabled = true
+
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomViewHolder {
+
         val layoutInflater = LayoutInflater.from(p0.context)
-        val itemView = layoutInflater.inflate(R.layout.fragment_list_item,p0,false)
-        return CustomViewHolder(itemView)
+        val itemView = layoutInflater.inflate(R.layout.fragment_list_item, p0, false)
+        return  CustomViewHolder(itemView)
+
     }
 
     override fun getItemCount(): Int {
@@ -25,8 +35,12 @@ class NeverEndingListAdapter(val jokes: List<Value>?): RecyclerView.Adapter<Neve
 
     }
 
+
     class CustomViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
+
         val mJokes:TextView = itemView.findViewById(R.id.never_ending_list_joke)
 
     }
+
+
 }
